7ae9772b99bfd5f3ffe990b673312f71a7a9a0f7
Kashish <kashishgupta142@gmail.com>
2023-01-04 22:17:32 +0530

    change

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,11 @@
-/*!
-    Title: Dev Portfolio Template
-    Version: 1.2.2
-    Last Change: 03/25/2020
-    Author: Ryan Fitzgerald
-    Repo: https://github.com/RyanFitzgerald/devportfolio-template
-    Issues: https://github.com/RyanFitzgerald/devportfolio-template/issues
 
-    Description: This file contains all the scripts associated with the single-page
-    portfolio website.
-*/
-
-(function($) {
-
-    // Show current year
+(function ($) {
     $("#current-year").text(new Date().getFullYear());
 
-    // Remove no-js class
     $('html').removeClass('no-js');
 
-    // Animate to section when nav is clicked
-    $('header a').click(function(e) {
+    $('header a').click(function (e) {
 
-        // Treat as normal link if no-scroll class
         if ($(this).hasClass('no-scroll')) return;
 
         e.preventDefault();
@@ -32,67 +16,67 @@
             scrollTop: scrollDistance + 'px'
         }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
 
-        // Hide the menu once clicked if mobile
+
         if ($('header').hasClass('active')) {
             $('header, body').removeClass('active');
         }
     });
 
-    // Scroll to top
-    $('#to-top').click(function() {
+
+    $('#to-top').click(function () {
         $('html, body').animate({
             scrollTop: 0
         }, 500);
     });
 
-    // Scroll to first element
-    $('#lead-down span').click(function() {
+
+    $('#lead-down span').click(function () {
         var scrollDistance = $('#lead').next().offset().top;
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
         }, 500);
     });
 
-    // Create timeline
-    $('#experience-timeline').each(function() {
 
-        $this = $(this); // Store reference to this
-        $userContent = $this.children('div'); // user content
+    $('#experience-timeline').each(function () {
 
-        // Create each timeline block
-        $userContent.each(function() {
+        $this = $(this);
+        $userContent = $this.children('div');
+
+
+        $userContent.each(function () {
             $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
         });
 
-        // Add icons to each block
-        $this.find('.vtimeline-point').each(function() {
+
+        $this.find('.vtimeline-point').each(function () {
             $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
         });
 
-        // Add dates to the timeline if exists
-        $this.find('.vtimeline-content').each(function() {
+
+        $this.find('.vtimeline-content').each(function () {
             var date = $(this).data('date');
-            if (date) { // Prepend if exists
-                $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
+            if (date) {
+                $(this).parent().prepend('<span class="vtimeline-date">' + date + '</span>');
             }
         });
 
     });
 
-    // Open mobile menu
-    $('#mobile-menu-open').click(function() {
+
+    $('#mobile-menu-open').click(function () {
         $('header, body').addClass('active');
     });
 
-    // Close mobile menu
-    $('#mobile-menu-close').click(function() {
+
+    $('#mobile-menu-close').click(function () {
         $('header, body').removeClass('active');
     });
 
-    // Load additional projects
-    $('#view-more-projects').click(function(e){
+
+    $('#view-more-projects').click(function (e) {
         e.preventDefault();
-        $(this).fadeOut(300, function() {
+        $(this).fadeOut(300, function () {
             $('#more-projects').fadeIn(300);
         });
     });
